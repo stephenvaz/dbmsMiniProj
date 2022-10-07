16a0411c3e42e8a63b0c6f1c8036d5ebed5a5e8d
Stephen Vaz <54773712+stephenvaz@users.noreply.github.com>
2022-10-08 00:00:07 +0530

    dbTables created

---
+++ db/creation/createTest.sql
@@ -0,0 +1,24 @@
+
+--@block
+DESC Prison;
+DESC Prisoner;
+DESC EducationFacilites;
+DESC Budget;
+DESC Crime;
+DESC Rehab;
+DESC Education;
+DESC Tranquility;
+DESC Fatality;
+
+--@block
+SHOW TABLES
+
+--@block
+DROP TABLE Rehab;
+DROP TABLE Education;
+DROP TABLE Prison;
+DROP TABLE Prisoner;
+DROP TABLE EducationFacilites;
+DROP TABLE Tranquility;
+DROP TABLE Fatality;
+DROP TABLE Budget;
--- db/creation/dbTables.sql
+++ db/creation/dbTables.sql
@@ -1,36 +1,49 @@
-
 --@block
 DESC Prison;
-
 --@block
 SHOW TABLES;
-
 -- Siddhesh
 SHOW DATABASES;
 --@block
-CREATE TABLE Education (edq char(20) not NULL, noc int not NULL, nout int not NULL);
-
+CREATE TABLE Education(
+    edq varchar(50) not NULL,
+    noc int not NULL,
+    nout int not NULL,
+    sex VARCHAR(15) references Prisoner(sex),
+    PrisonState VARCHAR(50) NOT NULL references Prison(PrisonState),
+    dYear int NOT NULL references Prison(dYear)
+);
 --@block
-
-CREATE TABLE Budget (typ char(20) not NULL, amt int not NULL,  PrisonState VARCHAR(50) references Prison(PrisonState));
-
+CREATE TABLE Budget (
+    typ char(30) not NULL,
+    amt int not NULL,
+    PrisonState VARCHAR(50) references Prison(PrisonState),
+    dYear int NOT NULL references Prison(dYear)
+);
 --@block
-
-CREATE TABLE Crime (ctyp char(20) not NULL, agegroup INT NOT NULL references Prison(agegroup)
-, sex VARCHAR(50) references Prisoner(sex), dYear int NOT NULL references Prison(dYear)
+CREATE TABLE Crime (
+    ctyp varchar(50) not NULL,
+    agegroup VARCHAR(15) NOT NULL references Prison(agegroup),
+    sex VARCHAR(15) references Prisoner(sex),
+    PrisonState VARCHAR(50) NOT NULL references Prison(PrisonState),
+    dYear int NOT NULL references Prison(dYear),
+    convicted int NOT NULL,
+    underTrial int NOT NULL
+);
+--@block
+CREATE TABLE Rehab (
+    dYear int NOT NULL references Prison(dYear),
+    PrisonState VARCHAR(50) NOT NULL references Prison(PrisonState),
+    lap int not null,
+    fap int not NULL,
+    nopr int not NULL
 );
 
---@block
-
-CREATE TABLE Rehab (lap char(20) not NULL, fap char(20) not NULL, nopr int not NULL)
 
 
 
-DROP TABLE Prison;
-DROP TABLE Prisoner;
 
 
--- STEPHEN
 --@block
 CREATE TABLE Prison(
     PrisonName varchar(50) primary key,
@@ -38,47 +51,41 @@ CREATE TABLE Prison(
     PrisonState VARCHAR(50) NOT NULL,
     NoOfPrisoners INT NOT NULL CHECK (NoOfPrisoners >= 0),
     NoOfConvicts INT NOT NULL CHECK (NoOfConvicts >= 0),
-    NoofUnderTrial INT NOT NULL CHECK (NoofUnderTrial >= 0)
+    NoOfUnderTrial INT NOT NULL CHECK (NoofUnderTrial >= 0)
 );
-DESCRIBE Prison;
+--@block
 CREATE Table Prisoner(
-    agegroup VARCHAR(50) NOT NULL,
-    sex VARCHAR(50) NOT NULL,
+    agegroup VARCHAR(15) NOT NULL,
+    sex VARCHAR(15) NOT NULL,
     nationality VARCHAR(50) NOT NULL,
     religion VARCHAR(50) NOT NULL,
     caste VARCHAR(50) NOT NULL
 );
-
+--@block
 CREATE Table EducationFacilites(
-    CourseName VARCHAR(50) PRIMARY KEY, 
+    CourseName VARCHAR(50) PRIMARY KEY,
     NoOfInmates INT NOT NULL CHECK (NoOfInmates >= 0),
-    dYear INT NOT NULL references Prison(dYear)
+    dYear INT NOT NULL references Prison(dYear),
+    PrisonState VARCHAR(50) references Prison(PrisonState)
 );
-
+--@block
 CREATE Table Tranquility(
     typeof VARCHAR(50) NOT NULL,
     inmatesinjured INT NOT NULL CHECK (inmatesinjured >= 0),
     inmateskilled INT NOT NULL CHECK (inmateskilled >= 0),
     dYear INT NOT NULL references Prison(dYear),
     PrisonState VARCHAR(50) references Prison(PrisonState)
-
-);
-
+); 
+--@block
 CREATE TABLE Fatality(
     cause VARCHAR(50) NOT NULL,
-    sex VARCHAR(50) references Prisoner(sex),
-    dYear INT NOT NULL references Prison(dYear)
+    sex VARCHAR(15) references Prisoner(sex),
+    dYear INT NOT NULL references Prison(dYear),
+    PrisonState VARCHAR(50) references Prison(PrisonState),
+    noOfDeaths INT NOT NULL CHECK (noOfDeaths >= 0)
 );
 
---@block
-DESC Prison;
-DESC Prisoner;
-DESC EducationFacilites;
 
 
---@block
-DROP TABLE Prison;
-DROP TABLE Prisoner;
-DROP TABLE EducationFacilites;
-DROP TABLE Tranquility;
-DROP TABLE Fatality;
+
+
